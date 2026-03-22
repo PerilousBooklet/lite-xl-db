4881567c4b5ce91cbdf97413e882eb9f7f262253
PerilousBooklet <raffaele.orabona@protonmail.com>
2026-03-22 16:17:03 +0100

    wip: starting development

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # lite-xl-db
 A database manager integration for the Lite XL text editor.
+
+> [!NOTE]
+> This plugin is heavily WIP, not even in alpha state!
---
+++ init.lua
@@ -0,0 +1,11 @@
+--mod-version:3
+local core = require"core"
+local common = require"core.common"
+local config = require"core.config"
+
+-- TODO: Tableview is a tab view
+-- TODO: there can be multiple TableView instances (one table data object for eacg TableView instance)
+
+-- TODO: the table and the treeview must be searchable -> write commands with context as TableView
+-- TODO: make console to run SQL query (custom View in bottom drawer), show query code in left side, show query result in right side
+
